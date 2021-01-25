8c93c2b2de0a3c3b23b05e624a474c9836239861
perezsergioace <perez.sergio.ace@gmail.com>
2021-01-25 10:42:48 -0500

    adding if there are no items in the db, a new table with a message and spinner will show up.

--- src/Items/ItemList.js
+++ src/Items/ItemList.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Table from 'react-bootstrap/Table';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
 import { Link } from 'react-router-dom';
 
 const ItemList = (props) => {
@@ -42,6 +43,40 @@ const ItemList = (props) => {
         })
     }
 
+    if (items === undefined || items.length === 0) {
+        return (
+            <div style={{ width: '95%', margin: 'auto' }}>
+                <h1>Items</h1>
+                <Form>
+                    <Form.Control
+                        size='sm'
+                        type='text'
+                        placeholder='Search Items'
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                </Form>
+                <Table striped bordered hover responsive variant='dark'>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Item Name</th>
+                            <th>Item Type</th>
+                            <th className='text-center'>&#9881; Controls</th>
+                        </tr>
+                    </thead>
+                </Table>
+                <div style={{ display: 'flex', justifyContent: 'center', width: '100%', margin: 'auto' }}>
+                    <h1>No items in the database... </h1>
+                </div>
+                <div style={{ display: 'flex', justifyContent: 'center', width: '100%', margin: 'auto' }}>
+                    <Spinner animation="border" variant="danger">
+                        <span className="sr-only">Trouble Loading Item...</span>
+                    </Spinner>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div style={{ width: '95%', margin: 'auto' }}>
             <h1>Items</h1>
@@ -71,7 +106,7 @@ const ItemList = (props) => {
                             </td>
                             <td>{item.item_type}</td>
                             <td className='text-center'>
-                                <Button style={{margin: '10px 20px'}} variant='primary'>Edit</Button>
+                                <Button style={{ margin: '10px 20px' }} variant='primary'>Edit</Button>
                                 <Button variant='danger' onClick={() => removeItem(item.id)}>Delete</Button>
                             </td>
                         </tr>
